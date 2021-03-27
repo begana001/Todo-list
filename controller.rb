724b071d443f7afd589653567a525426bcee1491
Begana Choi <begana001.jpg@gmail.com>
2021-03-27 15:08:25 +0100

    chore(controller): create initialize, list, create methods

--- controller.rb
+++ controller.rb
@@ -1,2 +1,20 @@
+require_relative 'task'
+require_relative 'view'
+
 class Controller
+  def initialize(todo_list)
+    @todo_list = todo_list
+    @view = View.new
+  end
+
+  def list
+    tasks = @todo_list.all
+    @view.display(tasks)
+  end
+
+  def create
+    title = @view.ask_user_for_title
+    task = Task.new(title)
+    @todo_list.add(task)
+  end
 end
